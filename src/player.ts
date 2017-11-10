41bb1db3cc63765013b8d08e9d31a913f320096f
kgtkr <kgtkr.jp@gmail.com>
2017-11-10 22:20:58 +0900

    プレイヤーの速度修正

--- src/player.ts
+++ src/player.ts
@@ -7,20 +7,95 @@ export class Player extends Phaser.Text {
 
   update() {
     super.update();
-    if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-      this.x -= this.speed;
+    const left = this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT);
+    const right = this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT);
+    const up = this.game.input.keyboard.isDown(Phaser.Keyboard.UP);
+    const down = this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN);
+
+    const x: "l" | "r" | "c" = left && !right ? "l"
+      : !left && right ? "r"
+        : "c";
+
+    const y: "u" | "d" | "c" = up && !down ? "u"
+      : !up && down ? "d"
+        : "c";
+
+    let d: "c" | "l" | "r" | "u" | "d" | "ul" | "ur" | "dl" | "dr" = "c";
+
+    switch (x) {
+      case "l":
+        switch (y) {
+          case "u":
+            d = "ul";
+            break;
+          case "d":
+            d = "dl";
+            break;
+          case "c":
+            d = "l";
+            break;
+        }
+        break;
+      case "r":
+        switch (y) {
+          case "u":
+            d = "ur";
+            break;
+          case "d":
+            d = "dr";
+            break;
+          case "c":
+            d = "r";
+            break;
+        }
+        break;
+      case "c":
+        switch (y) {
+          case "u":
+            d = "u";
+            break;
+          case "d":
+            d = "d";
+            break;
+          case "c":
+            d = "c";
+            break;
+        }
+        break;
     }
 
-    if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-      this.x += this.speed;
-    }
+    const speed = this.speed;
+    const speedR = this.speed / Math.SQRT2;
 
-    if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-      this.y -= this.speed;
-    }
-
-    if (this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-      this.y += this.speed;
+    switch (d) {
+      case "l":
+        this.x -= speed;
+        break;
+      case "r":
+        this.x += speed;
+        break;
+      case "u":
+        this.y -= speed;
+        break;
+      case "d":
+        this.y += speed;
+        break;
+      case "ul":
+        this.x -= speedR;
+        this.y -= speedR;
+        break;
+      case "ur":
+        this.x += speedR;
+        this.y -= speedR;
+        break;
+      case "dl":
+        this.x -= speedR;
+        this.y += speedR;
+        break;
+      case "dr":
+        this.x += speedR;
+        this.y += speedR;
+        break;
     }
   }
 }
