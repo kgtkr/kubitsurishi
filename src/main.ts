1944f6dcc2f164fd9eb2f1dae88ab45932604627
kgtkr <kgtkr.jp@gmail.com>
2017-11-12 10:43:16 +0900

    フィット

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,11 @@ import { MainState } from "./main-state";
 const game = new Phaser.Game(800, 600, Phaser.AUTO, '');
 game.state.add("mainState", MainState);
 window.onload = () => {
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.refresh();
     game.state.start("mainState");
-}
+};
+
+window.onresize = () => {
+    game.scale.refresh();
+};
